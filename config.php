1e94700d078d8a466b1abf2b57599857fa43a340
Fabio Díaz <fabioedp15@gmail.com>
2023-11-21 13:56:51 -0600

    newdeploy
    fistpackage

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "skillsire-new-server.mysql.database.azure.com";
+$sql_db_port = "3306";
+//$sql_db_flag = "MYSQLI_CLIENT_SSL";
+// MySQL Database User
+$sql_db_user = "skillsire3050";
+// MySQL Database Password
+$sql_db_pass = "Infrax3050@";
+// MySQL Database Name
+$sql_db_name = "mySkillsire";
+
+// Site URL
+$site_url = "https://www.skillsire.com"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "73dcbae7-7673-4ecb-8795-7cb21f042e91"; // Your purchase code, don't give it to anyone. 
+?>
